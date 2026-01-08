9fcdac6c642a5c2952a5817cde605af5198c97e3
Alex <81509258+alexuuhat@users.noreply.github.com>
2026-01-08 21:29:13 +0600

    Add script to send Windows login alerts to Telegram

---
+++ winloginalert_to_telegram.py
@@ -0,0 +1,23 @@
+import requests
+import socket
+import datetime
+
+BOT_TOKEN = "<TOKEN>"
+CHAT_ID = "<Your Chat IT Here>"
+
+hostname = socket.gethostname()
+time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Get public IP
+try:
+    public_ip = requests.get("https://api.ipify.org", timeout=5).text
+except:
+    public_ip = "N/A"
+
+msg = f"🟢 Windows Login Detected\nHost: {hostname}\nPublic IP: {public_ip}\nTime: {time_now}"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": msg
+})
