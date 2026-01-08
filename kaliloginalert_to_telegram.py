8ebb927c63cc2a4b0d943262806316ac2c967b65
Alex <81509258+alexuuhat@users.noreply.github.com>
2026-01-08 21:25:45 +0600

    Add Telegram alert for Linux root login detection

---
+++ kaliloginalert_to_telegram.py
@@ -0,0 +1,23 @@
+import requests
+import socket
+import datetime
+
+BOT_TOKEN = "8158971349:AAG7XmiejAwekjWXkrcqI1LHbcFDeq7K8m0"
+CHAT_ID = "1826429464"
+
+hostname = socket.gethostname()
+time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Get public IP
+try:
+    public_ip = requests.get("https://api.ipify.org", timeout=5).text
+except:
+    public_ip = "N/A"
+
+msg = f"🟢 Linux root Login Detected\nHost: {hostname}\nPublic IP: {public_ip}\nTime: {time_now}"
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": msg
+})
